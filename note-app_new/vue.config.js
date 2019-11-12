ea488064d5a80fbcc7ce101c9a9d48a631fb19f3
Yurij Ol <2010nwa@gmail.com>
2019-11-12 14:12:33 +0200

    dist

--- note-app_new/vue.config.js
+++ note-app_new/vue.config.js
@@ -1,8 +1,8 @@
-// vue.config.js
+
 module.exports = {
-  // options...
+  
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/note-app_new/'
+    ? '/'
     : '/',
   assetsDir: '',
   filenameHashing: true,
